40c77b9886fc101a7ee7a4d4cf8982cf8069395c
James Reynolds <magnusviri@users.noreply.github.com>
2020-06-09 13:07:54 -0600

    Update init.lua

--- textadept/init.lua
+++ textadept/init.lua
@@ -1,3 +1,10 @@
+local m_buffer = textadept.menu.menubar[_L['Buffer']]
+local keys, OSX, GUI, CURSES, _L = keys, OSX, not CURSES, CURSES, _L
+
+-- Switch tab switching to what's common on macOS
+keys[OSX and 'm}' or 'mn'] = m_buffer[_L['Next Buffer']][2]
+keys[OSX and 'm{' or 'mp'] = m_buffer[_L['Previous Buffer']][2]
+
 buffer.use_tabs = true
 buffer.tab_width = 4
 textadept.editing.auto_pairs = nil
